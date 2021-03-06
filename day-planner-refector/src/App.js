e3f14fd1a863c20eb116daa785782f8694a96032
Angelee Shaw <angeleefshaw@gmail.com>
2021-03-06 13:40:44 -0500

    Getting state set up

--- day-planner-refector/src/App.js
+++ day-planner-refector/src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import Header from "./components/Header"
 import Tasks from "./components/Tasks"
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import './styles/App.css';
 
 function App() {
   return (
     <div>
       <Header />
-      <Tasks />
+      <Tasks time="9am"/>
+      <Tasks time="10am"/>
+      <Tasks time="11am"/>
     </div>
   )
 }
--- day-planner-refector/src/components/Tasks.js
+++ day-planner-refector/src/components/Tasks.js
@@ -1,74 +1,49 @@
-import React from 'react';
-import { Form, Col, Button } from "react-bootstrap";
-import { render } from 'react-dom';
+
+import React, { useState } from 'react';
+import { Form, Col } from "react-bootstrap";
 import '../styles/Tasks.css';
 
 
-class Tasks extends React.Component () {
-  state = {
-    value: 0
-  };
+ function Tasks(props) {
+   const [taskState, setTaskState] = useState({
+     list:""
+    });
 
-  handleSave =() => {
-    const input = this.val().trim()
-    console.log(input)
+ 
+    return (
+      <div className= "tasks">
+        <Form className="col-one-content save-item">
+            <Form.Row className="row">
+              <Col className = "col-1">
+                <Form.Control placeholder={props.time} />
+              </Col>
+              <Col  className="col-8">
+                <Form.Control placeholder="Enter Task Here"  />
+              </Col>
+              <Col className="col-1">
+                <button onClick = {() => setTaskState({...taskState, list: "where are you"})} type="click" className="form-control save-btn"><i className="fas fa-save"></i></button>
+              </Col>
+            </Form.Row>
+            <br></br>
+            <div className="col-10 smll-row">
+              <h3>My Tasks:</h3>
+              <ol> 
+              {taskState.list}
+              </ol>
+            </div>
+          </Form>
+      </div>
+    );
   }
 
-  render() {
-  return (
-    <div className= "tasks">
-      <Form className="col-one-content save-item">
-        <Form.Row className="row">
-          <Col className = "col-1">
-            <Form.Control placeholder="9am" />
-          </Col>
-          <Col  className="col-8">
-            <Form.Control placeholder="Enter Task Here" />
-          </Col>
-          <Col className="col-1">
-            <button type="click" className="form-control save-btn" onClick={this.handleSave}><i className="fas fa-save"></i></button>
-          </Col>
-        </Form.Row>
-        <br></br>
-        <div className="col-10 smll-row">
-          <h3>My Tasks:</h3>
-          <ol> 
-          </ol>
-        </div>
-      </Form>
-    </div>
-  )
-}
-}
 
 export default Tasks;
 
 
+// onClick={() => this.addtask()}
+//value = {this.state.tasks}
 
 
-// <body>
-// <div className="container">
-//   <Form className="col-one-content save-item" >
-//      <div className="form-row rows">
-//        <div className="col-1">
-//          <input type="text" className="form-control" placeholder="9am"></input>
-//        </div>
-//        <div classNameNAme="col-10">
-//          <input type="text" className="form-control task" placeholder=""></input>
-//        </div>
-   
-//        <div className="col-1">
-//          <button type="click" className="form-control save-btn"><i className="fas fa-save"></i></button> 
-//        </div>
-//      </div>
-//      <br></br>
-//      <div className="col-10 smll-row">
-//        <h3>My Tasks:</h3>
-//          <ol> 
-//          </ol>
-//      </div>
-//    </Form>
-//  </div>
-// </body>
 
-     
+//
+//
