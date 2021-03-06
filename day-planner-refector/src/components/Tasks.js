4ad1122b342e047a98e288ccecf7a6e3f3892c8f
Angelee Shaw <angeleefshaw@gmail.com>
2021-03-06 14:03:43 -0500

    Update Tasks.js

--- day-planner-refector/src/components/Tasks.js
+++ day-planner-refector/src/components/Tasks.js
@@ -5,30 +5,37 @@ import '../styles/Tasks.css';
 
 
  function Tasks(props) {
-   const [taskState, setTaskState] = useState({
-     list:""
-    });
+   const [taskState, setTaskState] = useState("")
+
+     function handleSubmit(e) {
+       e.preventDefault();
+     }
+
+     function handleTask (e) {
+       setTaskState(e.target.value);
+     }
+    
 
  
     return (
       <div className= "tasks">
-        <Form className="col-one-content save-item">
+        <Form className="col-one-content save-item" >
             <Form.Row className="row">
               <Col className = "col-1">
                 <Form.Control placeholder={props.time} />
               </Col>
               <Col  className="col-8">
-                <Form.Control placeholder="Enter Task Here"  />
+                <Form.Control placeholder="Enter Task Here" onChange = {handleTask} />
               </Col>
               <Col className="col-1">
-                <button onClick = {() => setTaskState({...taskState, list: "where are you"})} type="click" className="form-control save-btn"><i className="fas fa-save"></i></button>
+                <button onClick = {handleSubmit} type="click" className="form-control save-btn"><i className="fas fa-save"></i></button>
               </Col>
             </Form.Row>
             <br></br>
             <div className="col-10 smll-row">
               <h3>My Tasks:</h3>
               <ol> 
-              {taskState.list}
+              {taskState}
               </ol>
             </div>
           </Form>
