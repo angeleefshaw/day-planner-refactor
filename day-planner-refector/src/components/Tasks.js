89d7fd7c01d6be660d5a73e0905fddc87bb5d573
Angelee Shaw <angeleefshaw@gmail.com>
2021-03-07 22:01:32 -0500

    Task component
    clearning input field on submission
    
    adding tasks

--- day-planner-refector/src/components/Tasks.js
+++ day-planner-refector/src/components/Tasks.js
@@ -1,39 +1,41 @@
 
-import React, { useState } from 'react';
-import { Form, Col } from "react-bootstrap";
+import React, { useState, useRef } from 'react';
+import { Form, Col, Button } from "react-bootstrap";
 import '../styles/Tasks.css';
 
 
  function Tasks(props) {
-   const [taskState, setTaskState] = useState("")
+   const [taskState, setTaskState] = useState([])
+   const input = useRef();
 
      function handleSubmit(e) {
        e.preventDefault();
-     }
+       setTaskState([...taskState, input.current.value]);
+       e.target.reset();
 
-     function handleTask (e) {
-       setTaskState(e.target.value);
-     }
-    
+     };
 
- 
     return (
       <div className= "tasks">
-        <Form className="col-one-content save-item" >
+        <Form className="col-one-content save-item" onSubmit = {handleSubmit}  >
             <Form.Row className="row">
               <Col className = "col-1">
                 <Form.Control placeholder={props.time} />
               </Col>
               <Col  className="col-8">
-                <Form.Control placeholder="Enter Task Here" onChange = {handleTask} />
+                <Form.Control placeholder="Enter Task Here" ref={input}/>
               </Col>
               <Col className="col-1">
-                <button onClick = {handleSubmit} type="click" className="form-control save-btn"><i className="fas fa-save"></i></button>
+                <Button  type="submit" className="form-control save-btn"><i className="fas fa-save"></i></Button>
               </Col>
             </Form.Row>
             <br></br>
             <div className="col-10 smll-row task-list">
-            {taskState}
+              <ul className="task-list">
+                {taskState.map((task, b) => (
+                <li className="new-task" li key={b}>{task}</li>
+                ))}
+              </ul>
             </div>
           </Form>
       </div>
@@ -43,11 +45,3 @@ import '../styles/Tasks.css';
 
 export default Tasks;
 
-
-// onClick={() => this.addtask()}
-//value = {this.state.tasks}
-
-
-
-//
-//
